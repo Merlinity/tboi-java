89ce415191841a679816c5d87d6f1b2a01e4e6c2
Merlinity <merlinaubel@gmx.de>
2022-02-22 18:25:42 +0100

    Random und while-Schleife hinzugefügt

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int item = 0;
+        Random random = new Random();
+
+        while (item < 9) {
+            item = random.nextInt(10) + 1;
+            System.out.println("Item Nr. " + item);
+        }
+
+        System.out.println("Ende.");
     }
 }
